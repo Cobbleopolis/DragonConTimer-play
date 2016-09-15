2f06e6a1da17c487f3a65d0028559bcd027ae044
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-15 17:16:13 -0400

    Slight refactoring

--- public/javascripts/systemjs.config.js
+++ public/javascripts/systemjs.config.js
@@ -7,24 +7,24 @@
         baseUrl: '/javascripts',
         paths: {
             // paths serve as alias
-            'npm:': 'assets/lib/'
+            'assets:': 'assets/lib/'
         },
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
             app: 'assets/javascripts/app',
             // angular bundles
-            '@angular/core': 'npm:angular__core/bundles/core.umd.js',
-            '@angular/common': 'npm:angular__common/bundles/common.umd.js',
-            '@angular/compiler': 'npm:angular__compiler/bundles/compiler.umd.js',
-            '@angular/platform-browser': 'npm:angular__platform-browser/bundles/platform-browser.umd.js',
-            '@angular/platform-browser-dynamic': 'npm:angular__platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            '@angular/http': 'npm:angular__http/bundles/http.umd.js',
-            '@angular/router': 'npm:angular__router/bundles/router.umd.js',
-            '@angular/forms': 'npm:angular__forms/bundles/forms.umd.js',
+            '@angular/core': 'assets:angular__core/bundles/core.umd.js',
+            '@angular/common': 'assets:angular__common/bundles/common.umd.js',
+            '@angular/compiler': 'assets:angular__compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'assets:angular__platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser-dynamic': 'assets:angular__platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/http': 'assets:angular__http/bundles/http.umd.js',
+            '@angular/router': 'assets:angular__router/bundles/router.umd.js',
+            '@angular/forms': 'assets:angular__forms/bundles/forms.umd.js',
             // other libraries
-            'rxjs':                       'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+            'rxjs':                       'assets:rxjs',
+            'angular2-in-memory-web-api': 'assets:angular2-in-memory-web-api'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
