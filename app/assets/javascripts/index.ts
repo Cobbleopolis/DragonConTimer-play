1421db0ce8f9fafebb2238933b2f57cc6494e88f
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-19 16:01:30 -0400

    Added console class

--- app/assets/javascripts/index.ts
+++ app/assets/javascripts/index.ts
@@ -1,10 +1,11 @@
-let left = 60;
+import { Console } from 'models/console';
+let console = new Console('a', 60);
 
 $(function() {
     let start = new Date();
     let timer = setInterval(function() {
         decProgBar(start);
-        if(left == 0)
+        if(console.time == 0)
             clearInterval(timer);
     }, 250);
 });
@@ -12,8 +13,8 @@ $(function() {
 function decProgBar(start: Date) {
     let progBar = $("#progBar");
     let t = (new Date().getTime() - start.getTime()) / 1000; // Add divide by 60 for an hour (at a min rn).
-    left = Math.max(60 - t, 0);
-    progBar.attr("aria-valuenow", left);
-    progBar.css("width", (left / 60) * 100 + "%");
-    progBar.text(Math.floor(left) + "min");
+    console.time = Math.max(60 - t, 0);
+    progBar.attr("aria-valuenow", console.time);
+    progBar.css("width", (console.time / 60) * 100 + "%");
+    progBar.text(Math.floor(console.time) + "min");
 }
---
+++ app/assets/javascripts/models/console.ts
@@ -0,0 +1,10 @@
+export class Console {
+
+    id: string;
+    time: number;
+
+    constructor(id: string, time: number) {
+        this.id = id;
+        this.time = time;
+    }
+}
