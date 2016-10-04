632c04cb920b75d3d55f95a0a28009e96899e5e8
Cobbleopolis <cobbleopolis@gmail.com>
2016-10-03 21:18:04 -0400

    Got broadcasting working to websocket clients

--- app/sockets/StationSocket.scala
+++ app/sockets/StationSocket.scala
@@ -2,13 +2,29 @@ package sockets
 
 import akka.actor._
 
+import scala.collection.mutable.ArrayBuffer
+
 object StationSocket {
+    var connectedClients: ArrayBuffer[ActorRef] = ArrayBuffer[ActorRef]()
+
     def props(out: ActorRef) = Props(new StationSocket(out))
+
+    def broadcast(msg: String) = connectedClients.foreach(client => client ! msg)
 }
 
 class StationSocket(out: ActorRef) extends Actor {
+
+    StationSocket.connectedClients += out
+
     def receive = {
         case msg: String =>
-            out ! ("I received your message: " + msg)
+            if (msg == "goodbye")
+                self ! PoisonPill
+            StationSocket.broadcast(msg)
+    }
+
+    override def postStop(): Unit = {
+        super.postStop()
+        StationSocket.connectedClients.remove(StationSocket.connectedClients.indexOf(out))
     }
 }
